820dbeb784a783899aeeba6e0b3cb98101f7cbcd
Gulenko Vladimir <minimal1@meta.ua>
2018-12-03 23:24:42 +0200

    task with variables

---
+++ Variables/Task_0/task_0.js
@@ -0,0 +1,12 @@
+'use strict';
+// Объявите две переменные: guest и name
+// В переменную name запишите строку 'Mango'
+// Скопируйте значение из name в guest
+// Выведите в консоли значение переменной guest (должно вывести 'Mango')
+
+
+const name = "Mongo";
+let guest;
+guest = name;
+alert(guest);
+// console.log(guest); --- alternative
